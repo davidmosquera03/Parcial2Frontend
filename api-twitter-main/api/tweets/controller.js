48c5cccdffb186d7bbe7cf7b2f132eaff8f7925b
David Maury <damaury@uninorte.edu.co>
2025-04-20 15:27:11 -0500

    Reply, Like y Delete hechos

--- api-twitter-main/api/tweets/controller.js
+++ api-twitter-main/api/tweets/controller.js
@@ -6,9 +6,11 @@ const list = (req, res) => {
   const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
 
-  Tweet.find({}, ["content", "comments", "likes", "user", "createdAt"])
+  Tweet.find({}, ["content", "comments", "likes", "user", "createdAt", "replyTo"])
     .populate("user", ["name", "username"])
     .populate("comments.user", ["name", "username"])
+    // Popula el tweet original al que se respondió
+    .populate("replyTo", ["content", "user", "createdAt"])
     .limit(Number(limit))
     .skip(skip)
     .sort({ createdAt: -1 })
@@ -27,19 +29,26 @@ const list = (req, res) => {
     });
 };
 
-const create = (req, res) => {
-  const { content, userId } = req.body;
 
+const create = (req, res) => {
+  const { content, userId, replyTo } = req.body;
+
+  // Se arma el objeto tweet y, de existir, se agrega replyTo
   const tweet = {
     content,
     user: userId,
   };
+
+  if (replyTo) {
+    tweet.replyTo = replyTo;
+  }
 
   const newTweet = new Tweet(tweet);
   newTweet.save().then((tweetCreated) => {
     res.status(200).json(tweetCreated);
   });
 };
+
 
 const createComment = (req, res) => {
   const { comment, tweetId, userId } = req.body;
@@ -58,17 +67,33 @@ const createComment = (req, res) => {
     });
 };
 
-const likes = (req, res) => {
-  const { like, tweetId } = req.body;
+const likes = async (req, res) => {
+  try {
+    const { tweetId, userId } = req.body;
+    const tweet = await Tweet.findById(tweetId);
+    if (!tweet) {
+      return res.status(404).json({ message: "Tweet not found" });
+    }
 
-  Tweet.updateOne({ _id: tweetId }, { $inc: { likes: 1 } })
-    .then(() => {
-      res.status(200).json({ message: "ok" });
-    })
-    .catch((error) => {
-      res.status(500).json({ message: "not updated" });
-    });
+    // Convertimos los ObjectId a string para comparar fácilmente
+    const likesAsString = tweet.likes.map((id) => id.toString());
+    const userHasLiked = likesAsString.includes(userId);
+
+    if (userHasLiked) {
+      // El usuario ya dio like: se elimina su like
+      await Tweet.updateOne({ _id: tweetId }, { $pull: { likes: userId } });
+      return res.status(200).json({ message: "Like removed" });
+    } else {
+      // El usuario no ha dado like: se agrega su like
+      await Tweet.updateOne({ _id: tweetId }, { $addToSet: { likes: userId } });
+      return res.status(200).json({ message: "Like added" });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
+
 
 const destroyTweet = async (req, res) => {
   const { tweetId, userId } = req.body;
