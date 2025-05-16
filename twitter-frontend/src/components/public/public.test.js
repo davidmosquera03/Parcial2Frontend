0327825ed9d69fd551624cf4cb9da96acaa97d06
David Hernandez <davidmosquera@uninorte.edu.co>
2025-05-16 18:11:24 -0500

    added Public unit tests

--- twitter-frontend/src/components/public/public.test.js
+++ twitter-frontend/src/components/public/public.test.js
@@ -1,9 +1,36 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Public from ".";
+import "@testing-library/jest-dom";
+import * as AuthContext from "../../context/AuthContext"; // adjust this path
+
+// Mock the useAuth hook
+jest.mock("../../context/AuthContext", () => ({
+  useAuth: () => ({
+    setIsLoggedIn: jest.fn(),
+  }),
+}));
 
 describe("Public", () => {
-  it("shows up correctly", () => {
+  it("Has login and signup buttons", () => {
     render(<Public />);
+    expect(screen.getByRole("button", { name: "Log in" })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Sign up" })).toBeInTheDocument();
+  });
+
+  it("Renders Login component when Log in button is clicked", () => {
+    render(<Public />);
+    fireEvent.click(screen.getByRole("button", { name: "Log in" }));
+    expect(screen.getByText("Your are logging in")).toBeInTheDocument();
+    expect(screen.getByRole("textbox", { id: "username" })).toBeInTheDocument();
+    expect(screen.getByRole("textbox", { id: "password" })).toBeInTheDocument();
+  });
+
+  it("Renders Signup component when Signup button is clicked", () => {
+    render(<Public />);
+    fireEvent.click(screen.getByRole("button", { name: "Sign up" }));
+    expect(
+      screen.getByRole("button", { name: "Register" })
+    ).toBeInTheDocument();
   });
 });
